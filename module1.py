dc780e3017e50d67bb2d292ed6133dcf5fae2ddb
prashanthpaul <prashanth.dec12@gmail.com>
2017-07-17 21:08:05 +0530

    just added one line

---
+++ module1.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      User
+#
+# Created:     17/07/2017
+# Copyright:   (c) User 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+
+
+def main():
+    print('Hey this is my first program ')
+
+if __name__ == '__main__':
+    main()
